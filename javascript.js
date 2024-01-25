cc18cd353ec443ef2046f18ed6c3739f4554f739
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-01-25 14:48:13 -0500

    converted base code into functions, then calls methods to execute code

--- javascript.js
+++ javascript.js
@@ -1,14 +1,36 @@
 
-let userInput = prompt("Please enter ice cream flavors");
-let flavorArr = userInput.split(" ");
-const flavors = {};
-for(const flavor of flavorArr)
-    if(flavor in flavors === false)
-        flavors[flavor] = 1;
-    else
-        flavors[flavor] += 1;
+/**
+ * @returns array of words entered into prompt
+ */
+function getInput() {
+    let userInput = prompt("Please enter ice cream flavors");
+    return userInput.split(" ");
+}
+
+/**
+ * 
+ * @param {String[]} arr an array of Strings 
+ * @returns flavors that counts flavor types
+ */
+function getObj(flavorArray) {
+    const flavors = {};
+    for(const word of flavorArray)
+        if(word in flavors)
+            flavors[word] += 1;
+        else
+            flavors[word] = 1;
+    return flavors;
+}
+
+/**
+ * 
+ * @param {flavor{}} object that counts flavors
+ */
+function printFlavors(flavors) {
+    for(const flavor in flavors)
+        console.log(`${flavor} has ${flavors[flavor]}`);
+}
 
 
-for(const flavor in flavors)
-    console.log(flavors[flavor]);
+printFlavors(getObj(getInput()));
 
